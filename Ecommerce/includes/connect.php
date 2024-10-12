79f2a3a9879e5c763740bf81b0a49d3eaea1a539
Gurpreet Singh <145257772+Gurpreet-Singh17@users.noreply.github.com>
2024-10-12 20:44:29 +0530

    Add files via upload

---
+++ Ecommerce/includes/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+// $con=mysqli_connect('sql200.infinityfree.com','if0_35823260','2BxMpWSnPWxvT5m','if0_35823260_crudoperation');
+// if(!$con){
+//     die(mysqli_error($con));
+// }
+
+$con=mysqli_connect('localhost','root','','mystore');
+if(!$con){
+    die(mysqli_error($con));
+}
+
+?>
---
+++ Ecommerce/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="bg-info p-3 text-center">
+<p>All rights are reserved &copy; Designed by Gurpreet Singh </p>
+</div>
